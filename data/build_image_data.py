44bb7800945f95e2700c9107cd7e313b51c41ef9
beta <betasspace@gmail.com>
2019-01-20 14:35:19 +0800

    fix

--- data/build_image_data.py
+++ data/build_image_data.py
@@ -375,7 +375,7 @@ def _find_image_files(data_dir, labels_file):
 
   # Construct the list of JPEG files and labels.
   for label_name, text in unique_labels:
-    jpeg_file_path = '*/%s_*' % label_name
+    jpeg_file_path = '%s/%s*' % (data_dir, label_name)
     matching_files = tf.gfile.Glob(jpeg_file_path)
     print('matching_files:')
     print(matching_files[:2], '...')
