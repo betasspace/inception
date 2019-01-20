66491409f7026076308437a2e48606d0deb6a67d
beta <betasspace@gmail.com>
2019-01-20 14:30:02 +0800

    fix

--- data/build_image_data.py
+++ data/build_image_data.py
@@ -363,9 +363,9 @@ def _find_image_files(data_dir, labels_file):
     labels: list of integer; each integer identifies the ground truth.
   """
   print('Determining list of input files and labels from %s.' % data_dir)
-  unique_labels = [l.split(' ', 1) for l in tf.gfile.FastGFile(
+  unique_labels = [l.split(' ', 1) for l in tf.gfile.GFile(
       labels_file, 'r').readlines()]
-
+  print(unique_labels)
   labels = []
   filenames = []
   texts = []
